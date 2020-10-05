4b0ecb41a72fc1c40e9d6945e0ad00d968a7e693
luizhemelo <luizhemelo@gmail.com>
2020-10-05 17:20:48 -0300

    Commit 02

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    Tests test = new Tests();
+    test.testMultiplication();
+    test.testEquality();
+    test.testFrancMultiplication();
+  }
+}
